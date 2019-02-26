b836b30efbc6f6fc397c2f4d5d64c150ffad9ffd
Florent CHANAL <flopp26@users.noreply.github.com>
2019-02-26 15:20:50 +0100

    Create SeoTranslator.php

---
+++ Seo/SeoTranslator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Leogout\Bundle\SeoBundle\Seo;
+
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class SeoTranslator
+{
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * @param $content
+     * @return string
+     */
+    public function getTranslation($content)
+    {
+        if(strtoupper(substr($content, strlen($content) - 6, 6)) == '|TRANS'){
+            $content = str_replace('|trans', '', $content);
+            $content = $this->translator->trans($content);
+        }
+
+        return $content;
+    }
+}
