c8a39722bd3d9ee9ab5a00b04b8bd45947fd7a69
Florent CHANAL <flopp26@users.noreply.github.com>
2019-03-13 17:15:17 +0100

    Update SeoExtension.php

--- Twig/SeoExtension.php
+++ Twig/SeoExtension.php
@@ -6,6 +6,9 @@ use Leogout\Bundle\SeoBundle\Builder\ImageBuilder;
 use Leogout\Bundle\SeoBundle\Builder\TagBuilder;
 use Leogout\Bundle\SeoBundle\Model\RenderableInterface;
 use Leogout\Bundle\SeoBundle\Provider\SeoGeneratorProvider;
+use Knp\Menu\Twig\Helper as KnpMenuHelper;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * Description of SeoExtension.
@@ -30,16 +33,34 @@ class SeoExtension extends \Twig_Extension
     private $generatorProvider;
 
     /**
+     * @var KnpMenuHelper
+     */
+    private $menuHelper;
+
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
      * SeoExtension constructor.
      *
      * @param SeoGeneratorProvider $generatorProvider
      * @param TagBuilder $tagBuilder
      */
-    public function __construct(SeoGeneratorProvider $generatorProvider, TagBuilder $tagBuilder, ImageBuilder $imageBuilder)
+    public function __construct(SeoGeneratorProvider $generatorProvider, TagBuilder $tagBuilder, ImageBuilder $imageBuilder, KnpMenuHelper $menuHelper, RequestStack $requestStack, TranslatorInterface $translator)
     {
         $this->tagBuilder = $tagBuilder;
         $this->imageBuilder = $imageBuilder;
         $this->generatorProvider = $generatorProvider;
+        $this->menuHelper = $menuHelper;
+        $this->requestStack = $requestStack;
+        $this->translator = $translator;
     }
 
     /**
@@ -96,7 +117,32 @@ class SeoExtension extends \Twig_Extension
             }
         }
 
-        return $this->tagBuilder->render();
+        return $this->tagBuilder->render() . PHP_EOL . $this->generateBreadcrumbMarkup();
+    }
+
+    private function generateBreadcrumbMarkup()
+    {
+        if($currentItem = $this->menuHelper->getCurrentItem('website')){
+
+            $root = array(
+                '@context' => 'https://schema.org',
+                '@type' => 'BreadcrumbList',
+                'itemListElement' => array()
+            );
+
+            foreach($this->menuHelper->getBreadcrumbsArray($currentItem) as $index => $breadcrumb){
+                $root['itemListElement'][] =  array(
+                    '@type' => 'ListItem',
+                    'position' => $index + 1,
+                    'name' => $this->translator->trans($breadcrumb['item']->getName()),
+                    'item' => $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost() . $breadcrumb['item']->getUri()
+                );
+            }
+
+            return '<script type="application/ld+json">'. json_encode($root) .'</script>';
+        }
+
+        return;
     }
 
     /**
